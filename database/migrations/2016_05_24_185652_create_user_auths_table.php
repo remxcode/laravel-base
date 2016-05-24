87ddee0ab5c5cdfdeeb9ac9f0ab4546019d62ecd
Remx Xu <remx@foxmail.com>
2016-05-24 19:17:27 +0800

    feat: add UserAuth module

---
+++ database/migrations/2016_05_24_185652_create_user_auths_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserAuthsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_auths', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id')->nullable()->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->string('type')->default('')->comment('授权类型');
+            $table->string('openid')->default('');
+            $table->string('unionid')->default('')->comment('wechat unionid');;
+            $table->string('nickname')->default('')->comment('昵称');
+            $table->string('name')->default('')->comment('姓名');
+            $table->string('email')->default('')->comment('邮箱');
+            $table->string('avatar')->default('')->comment('头像');
+            $table->text('token');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('user_auths');
+    }
+}
