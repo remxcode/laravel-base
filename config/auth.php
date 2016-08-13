26af4587d4ff04908a38add5680037d297da80df
Remx Xu <2011150269@email.szu.edu.cn>
2016-08-13 04:34:29 -0400

    Applied fixes from StyleCI

--- config/auth.php
+++ config/auth.php
@@ -98,8 +98,8 @@ return [
     'passwords' => [
         'users' => [
             'provider' => 'users',
-            'table' => 'password_resets',
-            'expire' => 60,
+            'table'    => 'password_resets',
+            'expire'   => 60,
         ],
     ],
 
