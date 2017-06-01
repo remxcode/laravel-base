fccffd67ff2de17ce5ed0ae0f81e15cbb1874215
remxcode <remx@foxmail.com>
2017-06-01 17:04:49 +0800

    feat: update

---
+++ resources/lang/zh_CN/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => '密码长度须大于六位且与确认密码相同',
+    'reset'    => '密码已重置!',
+    'sent'     => '已发送密码重置邮件!',
+    'token'    => '密码重置口令无效',
+    'user'     => "此邮箱并未注册",
+
+];
